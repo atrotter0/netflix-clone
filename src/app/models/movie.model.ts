aa210808c6048fdb8039bd43417f013244f80f6a
Abel Trotter <a_trotter0@hotmail.com>
2018-08-17 17:19:41 -0700

    Add more movies to movieList property, render popular movies in view

--- src/app/models/movie.model.ts
+++ src/app/models/movie.model.ts
@@ -5,12 +5,18 @@ export class Movie {
     public title: string,
     public rating: string,
     public length: string,
-    public popular: boolean
+    public popular: boolean,
+    public imgUrl: string
   ) {}
 
   static movieList = {
-    disenchantment: new Movie('show', 'Comedy', 'Disenchantment', 'TV-14', '1 Part', true),
-    theDeparted: new Movie('movie', 'Drama', 'The Departed', 'R', '2h 31m', false)
+    disenchantment: new Movie('show', 'Comedy', 'Disenchantment', 'TV-14', '1 Part', true, './../../assets/img/thumbnails/disenchanted.jpg'),
+    theDeparted: new Movie('movie', 'Drama', 'The Departed', 'R', '2h 31m', false, './../../assets/img/thumbnails/the-departed.jpg'),
+    theOffice: new Movie('show', 'Comedy', 'The Office', 'TV-14', '9 Seasons', true, './../../assets/img/thumbnails/the-office.jpg'),
+    theDarkKnight: new Movie('movie', 'Action & Adventure', 'The Dark Knight', 'PG-13', '2h 32m', true, './../../assets/img/thumbnails/the-dark-knight.jpg'),
+    guardians2: new Movie('movie', 'Action & Adventure', 'Guardians of the Galaxy Vol. 2', 'PG-13', '2h 16m', true, './../../assets/img/thumbnails/guardians-vol-2.jpg'),
+    portlandia: new Movie('show', 'Comedy', 'Portlandia', 'TV-14', '8 Seasons', true, './../../assets/img/thumbnails/portlandia.jpg'),
+    lotrFellowship: new Movie('movie', 'Sci-Fi & Fantasy', 'Lord of the Rings: The Fellowship of the Ring', 'PG-13', '2h 58m', true, './../../assets/img/thumbnails/lotr-fellowship.jpg')
   };
 
   static findById(id: string) {
--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Movie } from '../models/movie.model';
 
 @Component({
   selector: 'app-movie-list',
   templateUrl: './movie-list.component.html',
   styleUrls: ['./movie-list.component.css']
 })
-export class MovieListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class MovieListComponent {
+  popularList: Movie[] = Movie.findPopular();
+  dramaList: Movie[] = Movie.findByCategory('Drama');
+  actionAdventureList: Movie[] = Movie.findByCategory('Action & Adventure');
+  scifiFantasyList: Movie[] = Movie.findByCategory('Sci-Fi & Fantasy');
+  horrorList: Movie[] = Movie.findByCategory('Horror');
+  crimeList: Movie[] = Movie.findByCategory('Crime');
+  sportsList: Movie[] = Movie.findByCategory('Sports');
+  documentaryList: Movie[] = Movie.findByCategory('Documentary');
+  familyList: Movie[] = Movie.findByCategory('Family');
+  //myList:
+  //watchItAgainList:
+  //newReleasesList:
 }
